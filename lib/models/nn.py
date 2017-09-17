f614f68398362df27fc2b429da78f87dadc79f21
Emma Strubell <emma.strubell@gmail.com>
2017-09-17 16:11:53 -0400

    fix?

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -1033,7 +1033,7 @@ class NN(Configurable):
             parse_preds[changed_cycle] = new_head
             tarjan.edges[new_head].add(changed_cycle)
             tarjan.edges[old_head].remove(changed_cycle)
-        return parse_preds, roots_lt, roots_gt, len_2_cycles, n_cycles
+      return parse_preds, roots_lt, roots_gt, len_2_cycles, n_cycles
     elif self.svd_tree:
       tokens_to_keep[0] = True
       length = np.sum(tokens_to_keep)
