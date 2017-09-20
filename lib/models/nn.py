13baff005ec925c279614d645fb616620b05ccb8
Emma Strubell <emma.strubell@gmail.com>
2017-09-20 13:00:14 -0400

    roots

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -950,14 +950,14 @@ class NN(Configurable):
     logits2D = tf.reshape(logits3D, tf.stack([batch_size * bucket_size, -1])) #* tokens_to_keep1D
 
 
-    logits2D = tf.Print(logits2D, [tf.shape(roots_mask), roots_mask], summarize=5000)
+    # logits2D = tf.Print(logits2D, [tf.shape(roots_mask), roots_mask], summarize=5000)
 
     roots_mask = tf.reshape(roots_mask, [batch_size*bucket_size, -1])
 
-    logits2D = tf.Print(logits2D, [tf.shape(roots_mask), roots_mask], summarize=5000)
-
-
-    logits2D = tf.Print(logits2D, [logits2D], summarize=5000)
+    # logits2D = tf.Print(logits2D, [tf.shape(roots_mask), roots_mask], summarize=5000)
+    #
+    #
+    # logits2D = tf.Print(logits2D, [logits2D], summarize=5000)
 
 
 
