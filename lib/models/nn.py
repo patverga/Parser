9c7056a92da4d903e1ec39b8bd4bdb42e13d3c4c
Emma Strubell <emma.strubell@gmail.com>
2017-10-23 17:51:23 -0400

    sinh

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -202,7 +202,7 @@ def additive_gate(q, k,
       logits += bias
     # weights = tf.nn.softmax(logits, name="gate_weights")
     # dropping out the attention links for each of the heads
-    weights_softmax = tf.nn.tanh(logits)
+    weights_softmax = tf.sinh(logits)
     return weights_softmax
 
 
