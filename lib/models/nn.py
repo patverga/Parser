4169e1a5b680ef1ceb6c64c6ad3309dd5985de7c
Emma Strubell <emma.strubell@gmail.com>
2017-11-07 18:07:35 -0500

    2d conv

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -28,7 +28,7 @@ from lib.models import rnn
 from configurable import Configurable
 from vocab import Vocab
 
-import scipy.linalg
+# import scipy.linalg
 
 def layer_norm(inputs, reuse, epsilon=1e-6):
   """Applies layer normalization.
