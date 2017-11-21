e35a4016ab5a2d2bdafc5bd851531f7c7c30e65b
Emma Strubell <emma.strubell@gmail.com>
2017-11-21 13:48:38 -0500

    use python svd

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -1197,9 +1197,9 @@ class NN(Configurable):
 
     # svd_loss = 0.
     # try:
-    s = tf.py_func(np.linalg.svd, [laplacian, False, False], [laplacian.dtype])
-    # with tf.device('/cpu:0'):
-    #   s = tf.svd(laplacian, compute_uv=False)
+    # s = tf.py_func(np.linalg.svd, [laplacian, False, False], [laplacian.dtype])
+    with tf.device('/cpu:0'):
+      s = tf.svd(laplacian, compute_uv=False)
 
     # this is the tol used in numpy.linalg.matrix_rank
     # tol = tf.reduce_max(s) * tf.cast(tf.reduce_max(tf.shape(laplacian)), tf.float32) * np.finfo(np.float32).eps
