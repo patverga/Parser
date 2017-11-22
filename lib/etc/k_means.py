ea4250afa2cf6107ccc8c61dcb6887c463a13b89
Emma Strubell <emma.strubell@gmail.com>
2017-11-22 11:35:12 -0500

    debug

--- lib/etc/k_means.py
+++ lib/etc/k_means.py
@@ -56,6 +56,7 @@ class KMeans(object):
     i = len(self._splits)-1
     while i > 0:
       while self._splits[i-1] >= self._splits[i] or self._splits[i-1] not in self._len_cntr:
+        print(self._splits[i-1], self._splits[i], self._len_cntr)
         self._splits[i-1] -= 1
       i -= 1
 
