d9435a13cef0d04dbce047ff394e2cdb850ce8c1
Emma Strubell <emma.strubell@gmail.com>
2017-12-13 16:40:24 -0500

    try to load conll2012

--- dataset.py
+++ dataset.py
@@ -103,12 +103,20 @@ class Dataset(Configurable):
     for i, sent in enumerate(buff):
       print(sent)
       for j, token in enumerate(sent):
-        word, tag1, tag2, head, rel = token[words.conll_idx], token[tags.conll_idx[0]], token[tags.conll_idx[1]], token[6], token[rels.conll_idx]
-        if rel == 'root':
-          head = j
-        else:
-          head = int(head) - 1
-        buff[i][j] = (word,) + words[word] + tags[tag1] + tags[tag2] + (head,) + rels[rel]
+        if self.conll:
+          word, tag1, tag2, head, rel = token[words.conll_idx], token[tags.conll_idx[0]], token[tags.conll_idx[1]], token[6], token[rels.conll_idx]
+          if rel == 'root':
+            head = j
+          else:
+            head = int(head) - 1
+          buff[i][j] = (word,) + words[word] + tags[tag1] + tags[tag2] + (head,) + rels[rel]
+        elif self.conll2012:
+          word, tag1, tag2, head, rel = token[words.conll_idx], token[tags.conll_idx], 'NN', 0, token[rels.conll_idx]
+          # if rel == 'root':
+          #   head = j
+          # else:
+          #   head = int(head) - 1
+          buff[i][j] = (word,) + words[word] + tags[tag1] + tags[tag2] + (head,) + rels[rel]
       # sent.insert(0, ('root', Vocab.ROOT, Vocab.ROOT, Vocab.ROOT, Vocab.ROOT, 0, Vocab.ROOT))
     return buff
   
