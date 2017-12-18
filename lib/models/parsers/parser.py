114317d6e0abcc6d35bb1cedac0acade0a7963dc
Emma Strubell <emma.strubell@gmail.com>
2017-12-17 21:52:37 -0500

    tune

--- lib/models/parsers/parser.py
+++ lib/models/parsers/parser.py
@@ -305,7 +305,7 @@ class Parser(BaseParser):
     output['svd_loss'] = arc_output['svd_loss']
     output['n_cycles'] = arc_output['n_cycles']
     output['len_2_cycles'] = arc_output['len_2_cycles']
-    output['cycles'] = arc_output['n_cycles'] > 1 or arc_output['len_2_cycles'] > 1
+    output['cycles'] = (arc_output['n_cycles'] > 1) or (arc_output['len_2_cycles'] > 1)
 
 
     #### OLD: TRANSFORMER ####
