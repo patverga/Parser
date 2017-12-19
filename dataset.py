4d08b56c4a659af8d8c647de3021f1398602700b
Emma Strubell <emma.strubell@gmail.com>
2017-12-19 12:45:55 -0500

    fix

--- dataset.py
+++ dataset.py
@@ -115,7 +115,7 @@ class Dataset(Configurable):
           buff[i][j] = (word,) + words[word] + tags[tag1] + tags[tag2] + (head,) + rels[rel]
         elif self.conll2012:
           # todo actually load predicted pos tags and parses
-          word, tag1, tag2, head, rel = token[words.conll_idx], token[tags.conll_idx[0]], token[tags.conll_idx[1]], token[5], token[rels.conll_idx]
+          word, tag1, tag2, head, rel = token[words.conll_idx], token[tags.conll_idx[0]], token[tags.conll_idx[1]], token[7], token[rels.conll_idx]
           if rel == 'root':
             head = j
           else:
