03d5da41aa77aca89a569c4e6c4690469c1485f4
Emma Strubell <emma.strubell@gmail.com>
2017-12-22 15:47:42 -0500

    srl eval

--- network.py
+++ network.py
@@ -351,8 +351,8 @@ class Network(Configurable):
         print("srl_preds", srl_preds)
         for i, (datum, word, pred) in enumerate(zip(data, words, preds)):
           word_str = word if trigger_label in pred else '-'
-          srl_strs = map(lambda p: self._vocabs[3][p], pred)
-          fields = (word_str,) + srl_strs
+          srl_strs = map(lambda p: self._vocabs[3][p][0], pred)
+          fields = (word_str,) + tuple(srl_strs)
           print(fields)
           owpl_str = '\t'.join(fields)
           f.write(owpl_str)
