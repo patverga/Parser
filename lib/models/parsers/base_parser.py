ba32ef7cc6c0059aba939b2cc84f862ebe0fe9bd
Emma Strubell <emma.strubell@gmail.com>
2017-12-28 15:00:57 -0500

    print

--- lib/models/parsers/base_parser.py
+++ lib/models/parsers/base_parser.py
@@ -117,8 +117,8 @@ class BaseParser(NN):
       sent[:,6] = rel_preds[tokens] # 7
       sent[:,7] = targets[tokens, 1] # 8
       sent[:,8] = targets[tokens, 2] # 9
-      print(sent[:,9:9+num_srls])
-      print(targets[tokens, non_srl_targets_len:num_srls+non_srl_targets_len])
+      # print(sent[:,9:9+num_srls])
+      # print(targets[tokens, non_srl_targets_len:num_srls+non_srl_targets_len])
       sent[:,9:9+num_srls] = targets[tokens, non_srl_targets_len:num_srls+non_srl_targets_len] # num_srls
       s_pred = srl_pred[tokens, :num_srls]
       if len(s_pred.shape) == 1:
