50ebc6fd1940a807f37a388cb8c06c41dcedee8c
Emma Strubell <emma.strubell@gmail.com>
2017-12-29 13:53:26 -0500

    check for unclosed parens

--- network.py
+++ network.py
@@ -368,10 +368,16 @@ class Network(Configurable):
         preds = all_predictions[bkt_idx][idx]
         words = all_sents[bkt_idx][idx]
         srl_preds = preds[:,9:]
+        unclosed_paren = [0]*srl_preds.shape[1]
         # print("srl_preds", srl_preds)
         for i, (datum, word, pred) in enumerate(zip(data, words, srl_preds)):
           word_str = word if self.trigger_idx in pred else '-'
           srl_strs = map(self.convert_bilou, pred)
+          for i, s in enumerate(srl_strs):
+            if srl_strs[0] == "(":
+              unclosed_paren[i] += 1
+            if srl_strs[-1] == ")":
+              unclosed_paren[i] -= 1
           num_srl_strs = len(srl_strs)
           fields = (word_str,) + tuple(srl_strs[int(num_srl_strs/2):])
           # print(fields)
