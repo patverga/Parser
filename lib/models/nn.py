4248b946efe717988b7a989cb742980d9c8aeadc
Emma Strubell <emma.strubell@gmail.com>
2017-12-29 11:18:14 -0500

    subsample outside

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -1041,7 +1041,7 @@ class NN(Configurable):
 
 
     # todo right now this masks ALL outside. want to subsample and pass in the rate
-    outside_mask = 1 - tf.scatter_nd(sampled_indices, tf.fill([num_to_sample], 1), [batch_size, bucket_size, bucket_size])
+    # outside_mask = 1 - tf.scatter_nd(sampled_indices, tf.fill([num_to_sample], 1), [batch_size, bucket_size, bucket_size])
 
     targ_empty_indices = tf.cast(tf.where(tf.equal(targets3D, 0)), tf.int32)
     targets_mask3D = tf.scatter_nd(targ_empty_indices, tf.fill([tf.shape(targ_empty_indices)[0]], 3), shape=tf.stack([batch_size, bucket_size, bucket_size]))
@@ -1050,7 +1050,7 @@ class NN(Configurable):
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits_transposed, labels=targets3D_masked)
     cross_entropy *= self.tokens_to_keep3D
     cross_entropy *= tf.transpose(self.tokens_to_keep3D, [0, 2, 1])
-    cross_entropy *= outside_mask
+    # cross_entropy *= outside_mask
 
     loss = tf.reduce_sum(cross_entropy) / self.n_tokens
 
