2c6722a4b862eb1c60b58222b4ed5062852c08e1
Emma Strubell <emma.strubell@gmail.com>
2017-12-30 13:58:22 -0500

    comment

--- lib/models/nn.py
+++ lib/models/nn.py
@@ -1052,6 +1052,8 @@ class NN(Configurable):
 
     cross_entropy = tf.Print(cross_entropy, [targets3D_masked], "targets3D_masked", summarize=1000)
     cross_entropy = tf.Print(cross_entropy, [tf.count_nonzero(targets3D_masked * tf.cast(om, tf.int32)), targets3D_masked * tf.cast(om, tf.int32)], "targets3D_masked masked", summarize=1000)
+    cross_entropy = tf.Print(cross_entropy, [tf.count_nonzero(targets3D_masked * tf.cast(om, tf.int32)), tf.gather_nd(targets3D_masked * tf.cast(om, tf.int32), tf.where(tf.not_equal(targets3D_masked * tf.cast(om, tf.int32), 0)))], "targets3D_masked masked gather", summarize=1000)
+
     cross_entropy = tf.Print(cross_entropy, [tf.reduce_sum(cross_entropy),cross_entropy], "cross entropy", summarize=1000)
 
     loss = tf.reduce_sum(cross_entropy) #/ self.n_tokens
