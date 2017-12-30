use correct masking

lib/models/nn.py
@@ -1045,9 +1045,10 @@ class NN(Configurable):
     targets3D_masked = targets3D + targets_mask3D
 
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits_transposed, labels=targets3D_masked)
-    cross_entropy *= self.tokens_to_keep3D
-    cross_entropy *= tf.transpose(self.tokens_to_keep3D, [0, 2, 1])
-    cross_entropy = cross_entropy * om
+
+    overall_mask = om * self.tokens_to_keep3D * tf.transpose(self.tokens_to_keep3D, [0, 2, 1])
+
+    cross_entropy *= overall_mask
 
     # cross_entropy = tf.Print(cross_entropy, [targets3D], "targets3D", summarize=5000)
     #
@@ -1061,21 +1062,15 @@ class NN(Configurable):
     # cross_entropy = tf.Print(cross_entropy, [tf.count_nonzero(tf.gather_nd(targets3D, tf.where(tf.not_equal(targets3D_masked, 3)))), tf.gather_nd(targets3D, tf.where(tf.not_equal(targets3D_masked, 3)))], "targets3D_masked gather", summarize=5000)
     # cross_entropy = tf.Print(cross_entropy, [tf.reduce_sum(cross_entropy),cross_entropy], "cross entropy", summarize=1000)
 
-    non_masked_indices = tf.where(tf.not_equal(targets3D_masked * tf.cast(om, tf.int32), 0))
+    non_masked_indices = tf.where(tf.not_equal(targets3D_masked * overall_mask, 0))
     non_masked_targets = tf.gather_nd(targets3D, non_masked_indices)
-    count = tf.cast(tf.count_nonzero(non_masked_targets), tf.float32) + 1
-
-    # cross_entropy = tf.Print(cross_entropy, [count], "count", summarize=1000)
-    # cross_entropy = tf.Print(cross_entropy, [tf.reduce_sum(cross_entropy)], "tf.reduce_sum(cross_entropy)", summarize=1000)
-
-
+    count = tf.cast(tf.count_nonzero(non_masked_targets), tf.float32) + 1  # smoothing go avoid divide by 0
     loss = tf.reduce_sum(cross_entropy) / count
 
     probabilities = tf.nn.softmax(logits_transposed)
     predictions = tf.cast(tf.argmax(logits_transposed, axis=-1), tf.int32)
 
     correct = tf.reduce_sum(tf.cast(tf.equal(tf.gather_nd(predictions, non_masked_indices), non_masked_targets), tf.float32))
-
 
     output = {
       'loss': loss,