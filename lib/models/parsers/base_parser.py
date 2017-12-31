af0c54bab7d0b51331e176ebe4f8a98dfac6b519
Emma Strubell <emma.strubell@gmail.com>
2017-12-30 21:45:48 -0500

    fixing the toher thing

--- lib/models/parsers/base_parser.py
+++ lib/models/parsers/base_parser.py
@@ -93,9 +93,9 @@ class BaseParser(NN):
       srl_pred = srl_pred[tokens]
       num_gold_srls = len(np.where(targets[tokens, non_srl_targets_len:] == trigger_idx)[0])
       num_pred_srls = len(np.where(srl_pred == trigger_idx)[0])
-      print("s_pred shape", srl_pred.shape)
-      print("num pred srls", num_pred_srls)
-      print("np.where(s_pred == trigger_idx)", np.where(srl_pred == trigger_idx))
+      # print("s_pred shape", srl_pred.shape)
+      # print("num pred srls", num_pred_srls)
+      # print("np.where(s_pred == trigger_idx)", np.where(srl_pred == trigger_idx))
 
       # print("num srls", num_srls)
       # print("where", np.where(targets[tokens, non_srl_targets_len:] == trigger_idx))
