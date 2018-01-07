1fa1e8e5964a3c6631a8105c049f3bde86db3ed4
Emma Strubell <emma.strubell@gmail.com>
2018-01-07 18:36:21 -0500

    fix tune script

--- tune-srl-parse.sh
+++ tune-srl-parse.sh
@@ -63,7 +63,7 @@ for lr in ${lrs[@]}; do
                                                                 for rep in `seq $reps`; do
                                                                     fname_append="$rep-$lr-$mu-$nu-$epsilon-$warmup_steps-$batch_size-$cnn_dim-$trans_layer-$num_head-$head_size-$relu_hidden_size-$role_mlp_size-$trigger_mlp_size-$trigger_pred_mlp_size-$parse_update_proportion-$load_pretrained"
                                                                     load_pretrained_str=""
-                                                                    if [[ $load_pretrained -eq "True" ]]; then
+                                                                    if [[ "$load_pretrained" == "True" ]]; then
                                                                         load_pretrained_str="--load_dir $load_dir --load"
                                                                     fi
                                                                     commands+=("srun --gres=gpu:1 --partition=titanx-long,m40-long --time=24:00:00 --mem=10000
